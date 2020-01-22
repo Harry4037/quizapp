27f46498b7272c630a4ce3e4382090e08205f75f
Nitin <nitin@rizilianttech.com>
2020-01-22 15:46:39 +0530

    'admin'

--- app/Http/Controllers/Admin/QuestionController.php
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use App\Models\Question;
