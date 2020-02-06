ea31954238cb94d01c5fe9d719bfd13c2e04d1b1
Nitin <nitin@rizilianttech.com>
2020-02-06 16:04:57 +0530

    'api'

--- app/Http/Controllers/Api/RankingController.php
+++ app/Http/Controllers/Api/RankingController.php
@@ -6,7 +6,10 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use App\Models\Question;
+use App\Models\Follow;
 use App\Models\UserAnswer;
+use Illuminate\Support\Arr;
 use App\Models\UserTestSeriesQuestionAnswer;
 
 class RankingController extends Controller {
@@ -19,7 +22,7 @@ class RankingController extends Controller {
      *
      *
      * @apiParam {String} user_id User ID.
-     * 
+     *
      * @apiSuccess {String} success true
      * @apiSuccess {String} status_code (200 => success, 404 => Not found or failed).
      * @apiSuccess {String} message Rank List.
@@ -61,10 +64,10 @@ class RankingController extends Controller {
         if (!$request->user_id) {
             return $this->errorResponse("User ID Missing.");
         }
-        $userAnswers = DB::select(DB::raw('SELECT SUM(correct_answer) as total_correct_answer, user_id 
+        $userAnswers = DB::select(DB::raw('SELECT SUM(correct_answer) as total_correct_answer, user_id
             FROM (SELECT count(is_correct) as correct_answer, user_id FROM `user_answers` WHERE is_correct = 1 GROUP BY user_id
             UNION
-            SELECT SUM(is_correct) as correct_answer, user_id FROM `user_test_series_question_answers` WHERE is_correct = 1 GROUP BY user_id) 
+            SELECT SUM(is_correct) as correct_answer, user_id FROM `user_test_series_question_answers` WHERE is_correct = 1 GROUP BY user_id)
             as user_correct_ans GROUP BY user_id ORDER BY total_correct_answer DESC'));
         $dataArray = [];
         $myRanking = 0;
@@ -93,5 +96,73 @@ class RankingController extends Controller {
         $dataArray['user']['rank_number'] = $myRankingNo;
         return $this->successResponse("Ranking list", $dataArray);
     }
+    /**
+     * @api {get} /api/leadership Leadership
+     * @apiHeader {String} Accept application/json.
+     * @apiName GetLeadership
+     * @apiGroup Rank
+     *
+     *
+     * @apiSuccess {String} success true
+     * @apiSuccess {String} status_code (200 => success, 404 => Not found or failed).
+     * @apiSuccess {String} message Leadership List.
+     * @apiSuccess {JSON} data object.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     *   {
+     *       "status": true,
+     *       "status_code": 200,
+     *       "message": "Leadership list",
+     *       "data":[
+     *              {
+     *               "user_id":2,
+     *               "name":"Ankit_creator",
+     *               "profile_pic":"http:\/\/127.0.0.1:8000\/img\/no-image.jpg",
+     *                "points":3
+     *               },
+     *               {
+     *               "user_id":5,
+     *               "name":"shantanu",
+     *               "profile_pic":"http:\/\/127.0.0.1:8000\/img\/no-image.jpg",
+     *               "points":0
+     *               },
+     *               {
+     *               "user_id":7,
+     *              "name":"Sonali",
+     *               "profile_pic":"http:\/\/127.0.0.1:8000\/storage\/profile_pic\/P4dx2wtCWVdOSL3j34DOZCiu6Kvn9uSy2dKsFpMw.jpeg",
+     *               "points":0
+     *               },
+     *               {
+     *               "user_id":9,
+     *               "name":"sonali",
+     *               "profile_pic":"http:\/\/127.0.0.1:8000\/img\/no-image.jpg",
+     *               "points":0
+     *           }
+     *       ]
+     *   }
+     *
+     */
+    public function leadership(Request $request) {
+        $creatorUser = User::where('user_type_id',2)->get();
+        $dataArray = [];
+        $myRanking = 0;
+        $myRankingNo = 0;
+        if ($creatorUser) {
+            foreach ($creatorUser as $k => $user) {
+                $question = Question::where('user_id', $user->id)->where('is_approve', 2)->count();
+                $followers = Follow::where('follow_user_id',$user->id)->count();
+                $total = $question + $followers;
+                $dataArray[$k]['user_id'] = $user->id;
+                $dataArray[$k]['name'] = $user ? $user->name : 'User';
+                $dataArray[$k]['profile_pic'] = $user ? $user->profile_pic : '';
+                $dataArray[$k]['points'] = $total;
 
+            }
+        } else {
+            $dataArray['users_leadership'] = [];
+        }
+        $dataArray1 = collect($dataArray)->SortByDesc('points');
+        return $this->successResponse("Leadership list", $dataArray1);
+    }
 }
