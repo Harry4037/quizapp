99a63a22d4b339ff1989e1c2b9e96b02254c82fd
Nitin <nitin@rizilianttech.com>
2020-02-06 17:36:01 +0530

    'api'

--- app/Http/Controllers/Api/RankingController.php
+++ app/Http/Controllers/Api/RankingController.php
@@ -162,7 +162,7 @@ class RankingController extends Controller {
         } else {
             $dataArray['users_leadership'] = [];
         }
-        $dataArray1 = collect($dataArray['users_leadership'])->SortByDesc('points');
+        $dataArray1['users_leadership'] = collect($dataArray['users_leadership'])->SortByDesc('points');
         return $this->successResponse("Leadership list", $dataArray1);
     }
 }
