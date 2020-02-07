eec8777c8316da5d8b5c95874335ea6009a26c65
Hariom <hariom@gmail.com>
2020-02-07 13:08:58 +0530

    rc

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,10 +7,37 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Question;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller {
 
     public function index(Request $request) {
+        $css = [
+            'bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css'
+        ];
+        $js = [
+            'bower_components/datatables.net/js/jquery.dataTables.min.js',
+            'bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js'
+        ];
+
+        $userAnswers = DB::select(DB::raw('SELECT SUM(correct_answer) as total_correct_answer, user_id
+            FROM (SELECT count(is_correct) as correct_answer, user_id FROM `user_answers` WHERE is_correct = 1 GROUP BY user_id
+            UNION
+            SELECT SUM(is_correct) as correct_answer, user_id FROM `user_test_series_question_answers` WHERE is_correct = 1 GROUP BY user_id)
+            as user_correct_ans GROUP BY user_id ORDER BY total_correct_answer DESC'));
+        $rankingArray = [];
+        if ($userAnswers) {
+            foreach ($userAnswers as $k => $userAnswer) {
+                $user = User::find($userAnswer->user_id);
+                $rankingArray[$k]['user_id'] = $userAnswer->user_id;
+                $rankingArray[$k]['name'] = $user ? $user->name : 'User';
+                $rankingArray[$k]['profile_pic'] = $user ? $user->profile_pic : '';
+                $rankingArray[$k]['total_correct_answer'] = $userAnswer->total_correct_answer;
+            }
+        } else {
+            $rankingArray = [];
+        }
+
         $totalQuestionCount = Question::count();
         $usersCount = User::where('user_type_id', 3)->count();
         $creatorCount = User::where('user_type_id', 2)->count();
@@ -18,6 +45,9 @@ class DashboardController extends Controller {
             'usersCount' => $usersCount,
             'creatorCount' => $creatorCount,
             'totalQuestionCount' => $totalQuestionCount,
+            'rankingArray' => $rankingArray,
+            'js' => $js,
+            'css' => $css,
         ]);
     }
 
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -21,46 +21,172 @@
                 <div class="info-box">
                     <a href="{{ route('admin.question.index')}}"> <span class="info-box-icon bg-aqua"><i class="fa fa-question"></i></span>
 
-                    <div class="info-box-content">
-                        <span class="info-box-text">Questions</span></a>
-                        <span class="info-box-number">{{$totalQuestionCount}}</span>
-                    </div>
-                    <!-- /.info-box-content -->
+                        <div class="info-box-content">
+                            <span class="info-box-text">Questions</span></a>
+                    <span class="info-box-number">{{$totalQuestionCount}}</span>
                 </div>
-                <!-- /.info-box -->
+                <!-- /.info-box-content -->
             </div>
-            <!-- /.col -->
-            <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="info-box">
-                    <a href="{{ route('admin.creator.index')}}"><span class="info-box-icon bg-yellow"><i class="fa fa-users"></i></span>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <a href="{{ route('admin.creator.index')}}"><span class="info-box-icon bg-yellow"><i class="fa fa-users"></i></span>
 
                     <div class="info-box-content">
                         <span class="info-box-text">Creator</span></a>
-                        <span class="info-box-number">{{$creatorCount}}</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                </div>
-                <!-- /.info-box -->
+                <span class="info-box-number">{{$creatorCount}}</span>
             </div>
-            <!-- /.col -->
-
-            <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="info-box">
-                    <a href="{{ route('admin.user.index')}}"> <span class="info-box-icon bg-green"><i class="ion ion-ios-people-outline"></i></span>
-
-                    <div class="info-box-content">
-                        <span class="info-box-text">Users</span></a>
-                        <span class="info-box-number">{{$usersCount}}</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                </div>
-                <!-- /.info-box -->
-            </div>
-
+            <!-- /.info-box-content -->
         </div>
-        <!-- /.row -->
-    </section>
-    <!-- /.content -->
+        <!-- /.info-box -->
+</div>
+<!-- /.col -->
+
+<div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="info-box">
+        <a href="{{ route('admin.user.index')}}"> <span class="info-box-icon bg-green"><i class="ion ion-ios-people-outline"></i></span>
+
+            <div class="info-box-content">
+                <span class="info-box-text">Users</span></a>
+        <span class="info-box-number">{{$usersCount}}</span>
+    </div>
+    <!-- /.info-box-content -->
+</div>
+<!-- /.info-box -->
+</div>
+
+</div>
+<!-- /.row -->
+
+<div class="row">
+    <div class="col-md-6">
+        <!-- DIRECT CHAT -->
+        <div class="box box-warning direct-chat direct-chat-warning">
+            <div class="box-header with-border">
+                <h3 class="box-title">User's Rank</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+
+                <table id="list" class="table table-bordered table-hover text-center">
+                    <thead>
+                        <tr>
+                            <th>Rank No.</th>
+                            <th>Profile Pic.</th>
+                            <th>UserName</th>
+                            <th>Ranking</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if($rankingArray)
+                        @foreach($rankingArray as $k=>$ranking)
+                        <tr>
+                            <td>{{ $k+1 }}</td>
+                            <td><img class="img-circle" width="50" src="{{ $ranking['profile_pic'] }}"></td>
+                            <td>{{ $ranking['name'] }}</td>
+                            <td>{{ $ranking['total_correct_answer'] }}</td>
+                        </tr>
+                        @endforeach
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
+
+            </div>
+            <!-- /.box-footer-->
+        </div>
+        <!--/.direct-chat -->
+    </div>
+    <!-- /.col -->
+
+    <div class="col-md-6">
+        <!-- USERS LIST -->
+<!--        <div class="box box-danger">
+            <div class="box-header with-border">
+                <h3 class="box-title">Latest Members</h3>
+
+                <div class="box-tools pull-right">
+                    <span class="label label-danger">8 New Members</span>
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
+                    </button>
+                </div>
+            </div>
+             /.box-header 
+            <div class="box-body no-padding">
+                <ul class="users-list clearfix">
+                    <li>
+                        <img src="dist/img/user1-128x128.jpg" alt="User Image">
+                        <a class="users-list-name" href="#">Alexander Pierce</a>
+                        <span class="users-list-date">Today</span>
+                    </li>
+                    <li>
+                        <img src="dist/img/user8-128x128.jpg" alt="User Image">
+                        <a class="users-list-name" href="#">Norman</a>
+                        <span class="users-list-date">Yesterday</span>
+                    </li>
+                    <li>
+                        <img src="dist/img/user7-128x128.jpg" alt="User Image">
+                        <a class="users-list-name" href="#">Jane</a>
+                        <span class="users-list-date">12 Jan</span>
+                    </li>
+                    <li>
+                        <img src="dist/img/user6-128x128.jpg" alt="User Image">
+                        <a class="users-list-name" href="#">John</a>
+                        <span class="users-list-date">12 Jan</span>
+                    </li>
+                    <li>
+                        <img src="dist/img/user2-160x160.jpg" alt="User Image">
+                        <a class="users-list-name" href="#">Alexander</a>
+                        <span class="users-list-date">13 Jan</span>
+                    </li>
+                    <li>
+                        <img src="dist/img/user5-128x128.jpg" alt="User Image">
+                        <a class="users-list-name" href="#">Sarah</a>
+                        <span class="users-list-date">14 Jan</span>
+                    </li>
+                    <li>
+                        <img src="dist/img/user4-128x128.jpg" alt="User Image">
+                        <a class="users-list-name" href="#">Nora</a>
+                        <span class="users-list-date">15 Jan</span>
+                    </li>
+                    <li>
+                        <img src="dist/img/user3-128x128.jpg" alt="User Image">
+                        <a class="users-list-name" href="#">Nadia</a>
+                        <span class="users-list-date">15 Jan</span>
+                    </li>
+                </ul>
+                 /.users-list 
+            </div>
+             /.box-body 
+            <div class="box-footer text-center">
+                <a href="javascript:void(0)" class="uppercase">View All Users</a>
+            </div>
+             /.box-footer 
+        </div>-->
+        <!--/.box -->
+    </div>
+    <!-- /.col -->
+</div>
+</section>
+<!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
+@endsection
+
+@section('script')
+<script>
+
+    var t = $('#list').DataTable({
+        lengthMenu: [[10, 25, 50], [10, 25, 50]],
+        searching: true,
+        sorting: false
+    });
+
+</script>
 @endsection
