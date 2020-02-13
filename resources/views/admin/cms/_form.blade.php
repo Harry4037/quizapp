c71b254dc94dae93620047343bf1cf5be480baa2
Nitin <nitin@rizilianttech.com>
2020-02-13 13:46:46 +0530

    'api'

--- resources/views/admin/cms/_form.blade.php
+++ resources/views/admin/cms/_form.blade.php
@@ -5,24 +5,31 @@
             <input type="text" class="form-control" name="page_name" id="page_name" value="@if(isset($cms)){{$cms->page_name}}@endif" readonly="">
         </div>
     </div>
-    <div class="form-group">
-        <label class="control-label col-md-4 col-sm-4 col-xs-12">Description <span class="error">*</span></label>
-        <div class="col-md-6 col-sm-8 col-xs-8">
-            <textarea class="form-control" name="description" id="description">@if(isset($cms)){{$cms->description}}@endif</textarea>
-        </div>
-    </div>
 
     @if($cms->id == 3)
     <div class="form-group">
-        <label class="control-label col-md-4 col-sm-4 col-xs-12">Mobile <span class="error">*</span></label>
+        <label class="control-label col-md-4 col-sm-4 col-xs-12">Email <span class="error">*</span></label>
         <div class="col-md-6 col-sm-8 col-xs-8">
-        <input type="text" class="form-control" name="mobile" id="mobile" value="@if(isset($cms)){{$cms->mobile}}@endif" >
+            <input type="email" class="form-control" name="description" id="description" value="@if(isset($cms)){{$cms->title}}@endif" >
         </div>
     </div>
     <div class="form-group">
-        <label class="control-label col-md-4 col-sm-4 col-xs-12">Email <span class="error">*</span></label>
+        <label class="control-label col-md-4 col-sm-4 col-xs-12">Mobile <span class="error">*</span></label>
         <div class="col-md-6 col-sm-8 col-xs-8">
-        <input type="email" class="form-control" name="email" id="email" value="@if(isset($cms)){{$cms->email}}@endif" >
+        <input type="text" class="form-control" name="mobile" id="mobile" value="@if(isset($cms)){{$cms->description}}@endif" >
+        </div>
+    </div>
+    @else
+    <div class="form-group">
+        <label class="control-label col-md-4 col-sm-4 col-xs-12">Description <span class="error">*</span></label>
+        <div class="col-md-6 col-sm-8 col-xs-8">
+            <textarea class="form-control" name="description" id="description">@if(isset($cms)){{$cms->title}}@endif</textarea>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-md-4 col-sm-4 col-xs-12">Mobile <span class="error">*</span></label>
+        <div class="col-md-6 col-sm-8 col-xs-8">
+        <input type="text" class="form-control" name="mobile" id="mobile" value="@if(isset($cms)){{$cms->description}}@endif" >
         </div>
     </div>
     @endif
