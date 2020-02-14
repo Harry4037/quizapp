88bdb3c2cfa1b39bd68474d96fac0965b42522ac
Hariom <hariom@gmail.com>
2020-02-14 13:27:00 +0530

    rc

--- database/seeds/QuestionTableSeeder.php
+++ database/seeds/QuestionTableSeeder.php
@@ -13,204 +13,226 @@ class QuestionTableSeeder extends Seeder {
 
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 2,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 2,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 2,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 2,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 2,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 1,
+            'exam_id' => 0,
             'subject_id' => 2,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 2,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 2,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 2,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 2,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 2,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 2,
+            'exam_id' => 0,
             'subject_id' => 1,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 2,
+            'exam_id' => 0,
             'subject_id' => 2,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         DB::table('questions')->insert([
             'user_id' => 1,
-            'exam_id' => 2,
+            'exam_id' => 0,
             'subject_id' => 2,
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'ques_image' => " ",
             'ques_time' => 20,
             'lang' => 1,
+            'is_approve' => 2,
         ]);
         
     }
