51b400957ce7517fd28cb4bff9b81ee6f5214ce0
Nitin <nitin@rizilianttech.com>
2020-02-19 15:53:50 +0530

    'api'

--- resources/views/admin/test-series/question-edit.blade.php
+++ resources/views/admin/test-series/question-edit.blade.php
@@ -50,7 +50,8 @@
                                 <div class="form-group">
                                     <label class="control-label col-md-4 col-sm-4 col-xs-12">Option{{$k+1}} <span class="error">*</span></label>
                                     <div class="col-md-4 col-sm-6 col-xs-6">
-                                        <input type="text" class="form-control" name="ans{{$k+1}}" value="{{$answer->description}}">
+                                        <input type="text" class="form-control" id="ans{{$k+1}}" name="ans{{$k+1}}" value="{{$answer->description}}">
+                                        <input type="hidden" class="form-control" id="ansval{{$k+1}}" name="ansval{{$k+1}}" value="{{$answer->id}}">
                                     </div>
                                 </div>
                                 @endforeach
@@ -119,93 +120,6 @@
                     min: 1
                 },
             },
-        });
-
-        $(document).on("keyup", "#total_question", function () {
-            var days = parseInt($("#total_question").val());
-            if (days < 0) {
-                $("#total_question").val(0);
-                return false;
-            }
-            var i;
-            $("#question_div").html('');
-            if (days > 0) {
-                for (i = 0; i < days; i++) {
-                    var quesNo = i + 1;
-                    var day_html = '<div class="box-footer">'
-                            + '<label class="control-label">Question ' + quesNo + ':-</label>'
-                            + '<div class="form-group">'
-                            + '<label class="control-label col-md-4 col-sm-4 col-xs-12">Question Name <span class="error">*</span></label>'
-                            + '<div class="col-md-4 col-sm-6 col-xs-6">'
-                            + '<textarea class="form-control" name="description[' + i + ']" ></textarea>'
-                            + '</div>'
-                            + '</div>'
-                            + '<div class="form-group">'
-                            + '<label class="control-label col-md-4 col-sm-4 col-xs-12">Select Question Image</label>'
-                            + '<div class="col-md-4 col-sm-6 col-xs-6">'
-                            + '<input type="file" class="form-control" name="ques_image[' + i + ']">'
-                            + '</div>'
-                            + '</div>'
-                            + '<div class="form-group">'
-                            + '<label class="control-label col-md-4 col-sm-4 col-xs-12">Question Time <span class="error">*</span></label>'
-                            + '<div class="col-md-4 col-sm-6 col-xs-6">'
-                            + '<select class="form-control" name="time[' + i + ']">'
-                            + '<option value="60" selected>1 Min</option>'
-                            + '<option value="90">1 Min 30Sec</option>'
-                            + '<option value="120">2 Min</option>'
-                            + '<option value="150">2 Min 30Sec</option>'
-                            + '<option value="180">3 Min</option>'
-                            + '</select>'
-                            + '</div>'
-                            + '</div>'
-                            + '<div class="form-group">'
-                            + '<label class="control-label col-md-4 col-sm-4 col-xs-12">Option1 <span class="error">*</span></label>'
-                            + '<div class="col-md-4 col-sm-6 col-xs-6">'
-                            + '<input  type="text" class="form-control" name="ans1[' + i + ']" value="">'
-                            + '</div>'
-                            + '</div>'
-                            + '<div class="form-group">'
-                            + '<label class="control-label col-md-4 col-sm-4 col-xs-12">Option2 <span class="error">*</span></label>'
-                            + '<div class="col-md-4 col-sm-6 col-xs-6">'
-                            + '<input  type="text" class="form-control" name="ans2[' + i + ']" value="">'
-                            + '</div>'
-                            + '</div>'
-                            + '<div class="form-group">'
-                            + '<label class="control-label col-md-4 col-sm-4 col-xs-12">Option3 <span class="error">*</span></label>'
-                            + '<div class="col-md-4 col-sm-6 col-xs-6">'
-                            + '<input  type="text" class="form-control" name="ans3[' + i + ']" value="">'
-                            + '</div>'
-                            + '</div>'
-                            + '<div class="form-group">'
-                            + '<label class="control-label col-md-4 col-sm-4 col-xs-12">Option4 <span class="error">*</span></label>'
-                            + '<div class="col-md-4 col-sm-6 col-xs-6">'
-                            + '<input  type="text" class="form-control" name="ans4[' + i + ']" value="">'
-                            + '</div>'
-                            + '</div>'
-                            + '<div class="form-group">'
-                            + '<label class="control-label col-md-4 col-sm-4 col-xs-12">Correct Option <span class="error">*</span></label>'
-                            + '<div class="col-md-4 col-sm-6 col-xs-6">'
-                            + '<select class="form-control" name="correct_answer[' + i + ']">'
-                            + '<option value="">Choose option</option>'
-                            + '<option value="opt1">option1</option>'
-                            + '<option value="opt2">option2</option>'
-                            + '<option value="opt3">option3</option>'
-                            + '<option value="opt4">option4</option>'
-                            + '</select>'
-                            + '</div>'
-                            + '</div>'
-                            + '</div>';
-                    $("#question_div").append(day_html);
-                    $("textarea[name='description[" + i + "]']").rules("add", {required: true});
-                    $("input[name='ans1[" + i + "]']").rules("add", {required: true});
-                    $("input[name='ans2[" + i + "]']").rules("add", {required: true});
-                    $("input[name='ans3[" + i + "]']").rules("add", {required: true});
-                    $("input[name='ans4[" + i + "]']").rules("add", {required: true});
-                    $("select[name='correct_answer[" + i + "]']").rules("add", {required: true});
-                }
-
-            }
-
         });
 
     });
