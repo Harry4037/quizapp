6061416220b2c0ba163e7bcb2d77528927aab081
Hariom <hariom@gmail.com>
2020-02-28 10:46:59 +0530

    rc

--- app/Http/Controllers/Api/QuestionController.php
+++ app/Http/Controllers/Api/QuestionController.php
@@ -163,43 +163,42 @@ class QuestionController extends Controller {
         }
         if (!$request->user_id) {
             $lang1 = $request->lang;
-        $questions = Question::select('questions.id', 'questions.user_id', 'questions.description', 'questions.ques_image', 'questions.ques_time', 'questions.created_at')
-            ->where(function ($query) use ($lang1, $request) {
-                $query->where('questions.lang', $lang1)
-                ->where('questions.is_approve', 2);
-            })
-            ->limit(10)
-            ->get();
-        $dataArray = [];
-        $totatlTime = 0;
-        foreach ($questions as $k => $question) {
-            $answers = Answer::where("question_id", $question->id)->get();
-            $isLike = UserQuestionLike::where(["question_id" => $question->id, "user_id" => $request->user_id])->first();
-            $user1 = User::where('id', $question->user_id)->withTrashed()->first();
-            $dataArray[$k]['id'] = $question->id;
-            $dataArray[$k]['user']['id'] = $user1 ? $user1->id : 0;
-            $dataArray[$k]['user']['name'] = $user1 ? $user1->name : '';
-            $dataArray[$k]['user']['profile_pic'] = $user1 ? $user1->profile_pic : '';
-            $dataArray[$k]['description'] = $question->description;
-            $dataArray[$k]['ques_image'] = $question->ques_image;
-            $dataArray[$k]['ques_time'] = $question->ques_time;
-            $dataArray[$k]['is_like'] = $isLike ? true : false;
+            $questions = Question::select('questions.id', 'questions.user_id', 'questions.description', 'questions.ques_image', 'questions.ques_time', 'questions.created_at')
+                    ->where(function ($query) use ($lang1, $request) {
+                        $query->where('questions.lang', $lang1)
+                        ->where('questions.is_approve', 2);
+                    })
+                    ->limit(10)
+                    ->get();
+            $dataArray = [];
+            $totatlTime = 0;
+            foreach ($questions as $k => $question) {
+                $answers = Answer::where("question_id", $question->id)->get();
+                $isLike = UserQuestionLike::where(["question_id" => $question->id, "user_id" => $request->user_id])->first();
+                $user1 = User::where('id', $question->user_id)->withTrashed()->first();
+                $dataArray[$k]['id'] = $question->id;
+                $dataArray[$k]['user']['id'] = $user1 ? $user1->id : 0;
+                $dataArray[$k]['user']['name'] = $user1 ? $user1->name : '';
+                $dataArray[$k]['user']['profile_pic'] = $user1 ? $user1->profile_pic : '';
+                $dataArray[$k]['description'] = $question->description;
+                $dataArray[$k]['ques_image'] = $question->ques_image;
+                $dataArray[$k]['ques_time'] = $question->ques_time;
+                $dataArray[$k]['is_like'] = $isLike ? true : false;
 
-            $date = Carbon::parse($question->created_at);
-            $date1 = $date->format("d-M-Y");
-            $time1 = $date->format("h:i A");
+                $date = Carbon::parse($question->created_at);
+                $date1 = $date->format("d-M-Y");
+                $time1 = $date->format("h:i A");
 
-            $dataArray[$k]['ques_date_time'] = $date1 . " at " . $time1;
-            $dataArray[$k]['answers'] = $answers;
-            $totatlTime += $question->ques_time;
+                $dataArray[$k]['ques_date_time'] = $date1 . " at " . $time1;
+                $dataArray[$k]['answers'] = $answers;
+                $totatlTime += $question->ques_time;
+            }
+            shuffle($dataArray);
+            $data['question_time'] = $totatlTime;
+            $data['questions'] = $dataArray;
+            $data['page'] = 1;
+            return $this->successResponse("Question list.", $data);
         }
-        shuffle($dataArray);
-        $data['question_time'] = $totatlTime;
-        $data['questions'] = $dataArray;
-        $data['page'] = 1;
-        return $this->successResponse("Question list.", $data);
-
-    }
         $user = User::find($request->user_id);
         $lang = 1;
         if ($user) {
@@ -209,9 +208,24 @@ class QuestionController extends Controller {
         $appPage = 0;
         $limit = 10;
         $requestPage = 0;
+
+        $userAnswerCount = UserAnswer::where("user_id", $request->user_id)->count();
+        $userTestSeriesAnswerCount = UserTestSeriesQuestionAnswer::where("user_id", $request->user_id)->count();
+        $userAnswerArray = [];
+        $userTestSeriesAnswerCountArray = [];
+        if ($userAnswerCount > 0) {
+            $userAnswerArray = UserAnswer::where("user_id", $request->user_id)->pluck("question_id")->toArray();
+        }
+        if ($userTestSeriesAnswerCount > 0) {
+            $userTestSeriesAnswerCountArray = UserTestSeriesQuestionAnswer::where("user_id", $request->user_id)->pluck("question_id")->toArray();
+        }
+
+        $userQuestionIds = array_unique(array_merge($userAnswerArray, $userTestSeriesAnswerCountArray));
+
+
         if ($request->flag == 1) {
             $data['page'] = 1;
-            
+
             //Questions for new user
             $page = $user->page_no * 10;
             $questions = Question::select('questions.id', 'questions.user_id', 'questions.description', 'questions.ques_image', 'questions.ques_time', 'questions.created_at')
@@ -225,19 +239,6 @@ class QuestionController extends Controller {
                     ->get();
 
             if ($questions->count() == 0) {
-                $userAnswerCount = UserAnswer::where("user_id", $request->user_id)->count();
-                $userTestSeriesAnswerCount = UserTestSeriesQuestionAnswer::where("user_id", $request->user_id)->count();
-                $userAnswerArray = [];
-                $userTestSeriesAnswerCountArray = [];
-                if ($userAnswerCount > 0) {
-                    $userAnswerArray = UserAnswer::where("user_id", $request->user_id)->pluck("question_id")->toArray();
-                }
-                if ($userTestSeriesAnswerCount > 0) {
-                    $userTestSeriesAnswerCountArray = UserTestSeriesQuestionAnswer::where("user_id", $request->user_id)->pluck("question_id")->toArray();
-                }
-
-                $userQuestionIds = array_unique(array_merge($userAnswerArray, $userTestSeriesAnswerCountArray));
-
                 $page = $user->skip_page_no * 10;
 
                 //if all the questions are finished then show skipped questions
